e1c11888d5aa04ce7c512d7dbaf70284c176bfcd
Terry Jia <terryjia88@gmail.com>
2026-06-16 10:07:04 -0400

    feat: add bounding boxes and colors widgets

---
+++ src/components/palette/WidgetColors.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="flex size-full items-center px-1"
+    data-testid="colors"
+    @pointerdown.stop
+  >
+    <PaletteSwatchRow v-model="modelValue" :max="MAX_COLORS" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import PaletteSwatchRow from './PaletteSwatchRow.vue'
+
+const MAX_COLORS = 16
+
+const modelValue = defineModel<string[]>({ default: () => [] })
+</script>
